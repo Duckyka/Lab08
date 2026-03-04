9a15a38e3c71f705f10b9b39d10b0632d14390c8
Duckyka <Duckyka7@gmail.com>
2026-03-03 17:54:46 -0700

    Added failing test for countCities

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -22,4 +22,14 @@ public class CustomListTest {
         list.deleteCity(calgary);
         assertFalse(list.hasCity(calgary));
     }
+
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary));
+
+        assertEquals(1, list.countCities());
+    }
 }
